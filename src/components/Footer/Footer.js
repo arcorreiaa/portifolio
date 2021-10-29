dabb35c2d542d42c1f70c2430d3b9f67787bbc1c
Alysson Rychard <arcorreiaa@gmail.com>
2021-10-28 22:25:46 -0300

    portifolio

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a href='https://github.com/arcorreiaa' className='link footer__link'>
+      Created By Alysson Rychard
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,68 @@
+const header = {
+  // ainda nao sei oq linkar nesse botao
+  homepage: '#',
+  title: 'AR.',
+}
+
+const about = {
+  // aqui tem um pouco de mim
+  name: 'Alysson Rychard',
+  role: 'Front End Developer',
+  description:
+    'I am a person very passionate about the technology area. I usually play and study in my spare time and I love motorcycles..',
+  resume: 'https://www.instagram.com/arcoofc/',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/arcorreiaa/',
+    github: 'https://github.com/arcorreiaa',
+  },
+}
+
+const projects = [
+  // alguns projetos
+  {
+    name: 'TerrorFlix',
+    description:
+      'a project where I listed the horror movies that I think are the best I watch. this project I learned to use a little bit of JQUERY and each banner from the movie is linked to its trailer on youtube',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    sourceCode: 'https://github.com/arcorreiaa/netflix-clone-terrorflix',
+    livePreview: 'https://arcorreiaa.github.io/netflix-clone-terrorflix/',
+  },
+  {
+    name: 'Lista de Tarefas',
+    description:
+      'a simple todolist to add, list and delete. in this project I had my first contact with TypeScript and the project is responsive',
+    stack: ['TypeScript', 'HTML', 'CSS'],
+    sourceCode: 'https://github.com/arcorreiaa/todo',
+    livePreview: 'https://arcorreiaa.github.io/todo/',
+  },
+  {
+    name: 'Calculadora Simples',
+    description:
+      'A calculator with just the basics. in this project I had a difficulty with JavaScript, but in the end I managed to resolve the errors and ended',
+    stack: ['HTML', 'CSS', 'JavaScript'],
+    sourceCode: 'https://github.com/arcorreiaa/calculadora',
+    livePreview: 'https://arcorreiaa.github.io/calculadora/',
+  },
+]
+
+const skills = [
+  // sempre ta atualizando
+
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Node',
+  'Git',
+  'Tailwindcss',
+  'Photoshop',
+  'Bootstrap',
+]
+
+const contact = {
+  // meu email
+  email: 'arcorreiaa@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
