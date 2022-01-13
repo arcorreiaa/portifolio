400d92cf25b57fba1db047e288866757df336708
Alysson Correia <arcorreiaa@gmail.com>
2022-01-13 20:16:29 -0300

    Update portifolio and update img on projects

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => (
-  <footer className='footer'>
-    <a href='https://github.com/arcorreiaa' className='link footer__link'>
+  <footer className="footer">
+    <a
+      href="https://www.linkedin.com/in/arcorreiaa/"
+      className="link footer__link"
+    >
       Created By Alysson Rychard
     </a>
   </footer>
-)
+);
 
-export default Footer
+export default Footer;
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,68 +1,84 @@
+import Terrorflix from "../src/assets/img/terrorflix.gif";
+import Todo from "../src/assets/img/todo.gif";
+import Calculadora from "../src/assets/img/calculadora.gif";
+import Gasto from "../src/assets/img/gastomen.gif";
+
 const header = {
   // atualizei e coloquei meu curriculum
-  homepage: 'https://drive.google.com/file/d/1u9sot07YrQxBxBMGhJl-Lthfi0RGyflB/view?usp=sharing',
-  title: 'AR.',
-}
+  homepage: "#",
+  title: "AR.",
+};
 
 const about = {
   // aqui tem um pouco de mim
-  name: 'Alysson Rychard',
-  role: 'Front End Developer',
+  name: "Alysson Rychard",
+  role: "Front End Developer",
   description:
-    'I am a person very passionate about the technology area. I usually play and study in my spare time and I love motorcycles..',
-  resume: 'https://www.instagram.com/arcoofc/',
+    "I am a person very passionate about the technology area. I usually play and study in my spare time and I love motorcycles..",
+  resume: "https://www.instagram.com/arcoofc/",
   social: {
-    linkedin: 'https://www.linkedin.com/in/arcorreiaa/',
-    github: 'https://github.com/arcorreiaa',
+    linkedin: "https://www.linkedin.com/in/arcorreiaa/",
+    github: "https://github.com/arcorreiaa",
   },
-}
+};
 
 const projects = [
   // alguns projetos
   {
-    name: 'TerrorFlix',
+    name: "TerrorFlix",
     description:
-      'a project where I listed the horror movies that I think are the best I watch. this project I learned to use a little bit of JQUERY and each banner from the movie is linked to its trailer on youtube',
-    stack: ['HTML', 'CSS', 'JavaScript'],
-    sourceCode: 'https://github.com/arcorreiaa/netflix-clone-terrorflix',
-    livePreview: 'https://arcorreiaa.github.io/netflix-clone-terrorflix/',
+      "a project where I listed the horror movies that I think are the best I watch. this project I learned to use a little bit of JQUERY and each banner from the movie is linked to its trailer on youtube",
+    stack: ["HTML", "CSS", "JavaScript"],
+    sourceCode: "https://github.com/arcorreiaa/netflix-clone-terrorflix",
+    livePreview: "https://arcorreiaa.github.io/netflix-clone-terrorflix/",
+    img: Terrorflix,
   },
   {
-    name: 'Lista de Tarefas',
+    name: "Lista de Tarefas",
     description:
-      'a simple todolist to add, list and delete. in this project I had my first contact with TypeScript and the project is responsive',
-    stack: ['TypeScript', 'HTML', 'CSS'],
-    sourceCode: 'https://github.com/arcorreiaa/todo',
-    livePreview: 'https://arcorreiaa.github.io/todo/',
+      "a simple todolist to add, list and delete. in this project I had my first contact with TypeScript and the project is responsive",
+    stack: ["TypeScript", "HTML", "CSS"],
+    sourceCode: "https://github.com/arcorreiaa/todo",
+    livePreview: "https://arcorreiaa.github.io/todo/",
+    img: Todo,
   },
   {
-    name: 'Calculadora Simples',
+    name: "Calculadora Simples",
     description:
-      'A calculator with just the basics. in this project I had a difficulty with JavaScript, but in the end I managed to resolve the errors and ended',
-    stack: ['HTML', 'CSS', 'JavaScript'],
-    sourceCode: 'https://github.com/arcorreiaa/calculadora',
-    livePreview: 'https://arcorreiaa.github.io/calculadora/',
+      "A calculator with just the basics. in this project I had a difficulty with JavaScript, but in the end I managed to resolve the errors and ended",
+    stack: ["HTML", "CSS", "JavaScript"],
+    sourceCode: "https://github.com/arcorreiaa/calculadora",
+    livePreview: "https://arcorreiaa.github.io/calculadora/",
+    img: Calculadora,
   },
-]
+  {
+    name: "Gasto Mensal",
+    description: "a mini expense control system made with TypeScript",
+    stack: ["HTML", "CSS", "TypeScript"],
+    sourceCode: "https://github.com/arcorreiaa/gasto-mensal",
+    livePreview: "https://arcorreiaa.github.io/gasto-mensal/",
+    img: Gasto,
+  },
+];
 
 const skills = [
   // sempre ta atualizando
 
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Node',
-  'Git',
-  'Tailwindcss',
-  'Photoshop',
-  'Bootstrap',
-]
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Node",
+  "Git",
+  "Tailwindcss",
+  "Photoshop",
+  "Bootstrap",
+];
 
 const contact = {
   // meu email
-  email: 'arcorreiaa@gmail.com',
-}
+  email: "arcorreiaa@gmail.com",
+};
 
-export { header, about, projects, skills, contact }
+export { header, about, projects, skills, contact };
